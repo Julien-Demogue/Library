19aa695a24a9b97408fc0e7384839c63260a088d
Julien Demogue <julien.demogue@sfr.fr>
2024-10-22 11:51:26 +0200

    Added user and book entities

---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { AfterInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @AfterInsert()
+    logInsert() {
+        console.log('User created with id ' + this.id)
+    }
+}
